63d3c58aabf21c57758bb9b18d816de4b42a24d9
Magistyo Raharjo <magistyo@pttimboel.com>
2026-07-13 11:40:22 +0700

    final version before i check the actual physical constraints

--- tool_holder_rak_geser/main.py
+++ tool_holder_rak_geser/main.py
@@ -3,80 +3,61 @@ from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, ge
 
 # physical constrains
 
-small_bar_width = 10
-big_bar_width = 20
+small_bar_width = 20
+big_bar_width = 40
 max_holder_width = 150
 max_holder_length = 300
+max_holder_height = 150
 
 # Editable variable
 
-fillet_radius = 1
-holder_wall = 5
+fillet_radius = 3
+holder_wall = 10
 holder_bottom = holder_wall
-lip_width = small_bar_width
+lip_length = small_bar_width/2
+lip_width = big_bar_width
 hook_thickness = 10
-holder_height = 100
 
 # Part Setup
 base = (cq.Workplane()
         .box(
             max_holder_width,
             max_holder_length,
-            holder_height+holder_bottom
+            max_holder_height
         ))
 
 base_cutter = (cq.Workplane()
         .box(
             max_holder_width-(holder_wall*2),
             max_holder_length-(holder_wall*2),
-            holder_height
+            max_holder_height-(holder_bottom)+10
         )
         .translate((0,0,holder_bottom)))
 
 base_lip = (cq.Workplane("XY")
             .box(
-                max_holder_width+(lip_width*2),
-                max_holder_length,
+                max_holder_width+(lip_length*2),
+                max_holder_length+(lip_width),
                 hook_thickness
             )
-            .translate((0,0,holder_height/2)))
+            .translate((0,lip_width/2,max_holder_height/2)))
 
-big_bar_hook = (cq.Workplane()
-        .box(
-            max_holder_width,
-            big_bar_width+hook_thickness,
-            hook_thickness*2
-        ))
 
-big_bar_hook_cutter = (cq.Workplane()
+hook = (cq.Workplane()
         .box(
-            max_holder_width,
-            big_bar_width,
+            max_holder_width+(lip_length*2),
+            hook_thickness,
             hook_thickness
         )
-        .translate((0,big_bar_width/2,-hook_thickness/2))) 
+        .translate((0,max_holder_length/2 + lip_width - hook_thickness/2,(max_holder_height/2)-hook_thickness)))
 
-big_bar_hook = big_bar_hook - big_bar_hook_cutter
-
-big_bar_hook = big_bar_hook.translate((0,-(max_holder_length/2+big_bar_width),holder_height/2))
-
-base = (base + base_lip) - base_cutter + big_bar_hook
-
-# base = (cq.Workplane("XZ")
-#         .moveTo(-max_holder_width/2,((holder_height+holder_bottom)/2))
-#         .line(0,-(holder_height + holder_bottom))
-#         .line(max_holder_width,0)
-#         .line(0,(holder_height + holder_bottom) - hook_thickness)
-#         .line(small_bar_width,0)
-#         .line(0,-small_bar_width)
-#         .line(hook_thickness,0)
-#         .line(0,small_bar_width+hook_thickness)
-#         .line(-(holder_wall+small_bar_width+hook_thickness),0)
-#         .line(0,-holder_height)
-#         .line(-(max_holder_width-(2*holder_wall)),0)
-#         .line(0,holder_height)
-#         .close()
-#         .extrude(max_holder_length/2 ,both=True))
+base = (
+    (base + base_lip)
+    .cut(base_cutter)
+    .union(hook)
+    .edges()
+    .fillet(fillet_radius)
+)
 
 
 
