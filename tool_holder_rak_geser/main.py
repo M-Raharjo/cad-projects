423c6133dad025dcb29ad90e5f37a326a8cb01dd
Magistyo Raharjo <magistyo@pttimboel.com>
2026-07-14 10:28:57 +0700

    final tool holder

--- tool_holder_rak_geser/main.py
+++ tool_holder_rak_geser/main.py
@@ -1,22 +1,24 @@
 import cadquery as cq
 from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, get_defaults
+from pathlib import Path
 
 # physical constrains
 
 small_bar_width = 20
-big_bar_width = 40
-max_holder_width = 150
-max_holder_length = 300
+big_bar_width = 30
+max_holder_width = 89
+max_holder_length = 200
 max_holder_height = 150
 
 # Editable variable
 
-fillet_radius = 3
-holder_wall = 10
+
+holder_wall = 5
 holder_bottom = holder_wall
 lip_length = small_bar_width/2
 lip_width = big_bar_width
-hook_thickness = 10
+hook_thickness = 2*holder_wall
+fillet_radius = holder_wall * 0.4
 
 # Part Setup
 base = (cq.Workplane()
@@ -37,10 +39,10 @@ base_cutter = (cq.Workplane()
 base_lip = (cq.Workplane("XY")
             .box(
                 max_holder_width+(lip_length*2),
-                max_holder_length+(lip_width),
+                max_holder_length+(lip_width+hook_thickness),
                 hook_thickness
             )
-            .translate((0,lip_width/2,max_holder_height/2)))
+            .translate((0,(lip_width+hook_thickness)/2,max_holder_height/2)))
 
 
 hook = (cq.Workplane()
@@ -49,16 +51,35 @@ hook = (cq.Workplane()
             hook_thickness,
             hook_thickness
         )
-        .translate((0,max_holder_length/2 + lip_width - hook_thickness/2,(max_holder_height/2)-hook_thickness)))
+        .translate((0,max_holder_length/2 + lip_width + hook_thickness/2,(max_holder_height/2)-hook_thickness)))
 
 base = (
     (base + base_lip)
     .cut(base_cutter)
     .union(hook)
     .edges()
-    .fillet(fillet_radius)
+    .chamfer(fillet_radius)
 )
 
 
 
 show(base)
+
+## Exporting
+
+# Folder containing this main.py file
+script_dir = Path(__file__).resolve().parent
+
+# Create exports folder if it does not exist
+export_dir = script_dir / "exports"
+export_dir.mkdir(parents=True, exist_ok=True)
+
+# Export STL
+export_path = export_dir / "holder.stl"
+
+cq.exporters.export(
+    base,
+    str(export_path),
+    tolerance=0.05,
+    angularTolerance=0.1,
+)
